3b1b199565e8a7b2c69215f0b6fcd8482932ecef
yudha490 <yudhapratama8944@gmail.com>
2024-03-21 11:06:23 +0700

    menambahkan class SimpleDataBase

--- modul5_1302223117/Program.cs
+++ modul5_1302223117/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class Penjumlahan
+{
+    public void JumlahTigaAngka<T>(T a, T b, T c)
+    {
+        dynamic angka1 = a;
+        dynamic angka2 = b;
+        dynamic angka3 = c;
+
+        dynamic hasil = angka1 + angka2 + angka3;
+
+        Console.WriteLine(hasil);
+    }
+}
+
+public class SimpleDataBase<T, Date>
+{ 
+    public T storedData { get; set; }
+    public Date inputDates { get; set; }
+    public SimpleDataBase(T data) 
+    {
+        storedData = data;
+    }
+
+    public void AddNewData<T>(T data)
+    {
+
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Penjumlahan penjumlahan = new Penjumlahan();
+        penjumlahan.JumlahTigaAngka<int>(13, 02, 22);
+    }
+}
